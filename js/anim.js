812cbce0fb41fb5261454d08e68796ad61aee520
taqi sammy <staqi@info-s1-02.iut-bm.univ-fcomte.fr>
2025-10-24 17:00:56 +0200

    Reboot de la page d'acceuil

--- js/anim.js
+++ js/anim.js
@@ -13,3 +13,16 @@ img.addEventListener("mouseenter", () => {
   });
 });
 
+const slides = document.querySelector('.slides');
+const images = document.querySelectorAll('.slides img');
+let index = 0;
+
+document.querySelector('.next').addEventListener('click', () => {
+  index = (index + 1) % images.length;
+  slides.style.transform = `translateX(-${index * 100}%)`;
+});
+
+document.querySelector('.prev').addEventListener('click', () => {
+  index = (index - 1 + images.length) % images.length;
+  slides.style.transform = `translateX(-${index * 100}%)`;
+});
